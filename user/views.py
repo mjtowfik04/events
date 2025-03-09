99f9762c60673ab429f609d3f345e07dcf3dd0db
towfik <mjtowfik659672@gmail.com>
2025-03-10 04:41:01 +0600

    all code

--- user/views.py
+++ user/views.py
@@ -46,7 +46,7 @@ def sign_in(request):
     return render(request, 'registration/login.html', {'form': form})
 
 
-# @login_required
+@login_required
 def sign_out(request):
     if request.method == 'POST':
         logout(request)
@@ -67,7 +67,7 @@ def activate_user(request, user_id, token):
         return HttpResponse('User not found')
 
 
-# @user_passes_test(is_admin, login_url='no-permission')
+@user_passes_test(is_admin, login_url='no-permission')
 def admin_dashboard(request):
     users = User.objects.prefetch_related(
         Prefetch('groups', queryset=Group.objects.all(), to_attr='all_groups')
@@ -83,25 +83,8 @@ def admin_dashboard(request):
     return render(request, 'admin/dashboard.html', {"users": users})
 
 
-# @user_passes_test(is_admin, login_url='no-permission')
-# def assign_role(request, user_id):
-#     user = User.objects.get(id=user_id)
-#     form = AssignRoleForm()
 
-#     if request.method == 'POST':
-#         form = AssignRoleForm(request.POST)
-#         if form.is_valid():
-#             role = form.cleaned_data.get('role')
-#             user.groups.clear()  # Remove old roles
-#             user.groups.add(role)
-#             messages.success(request, f"User {
-#                              user.username} has been assigned to the {role.name} role")
-#             return redirect('admin-dashboard')
-
-#     return render(request, 'admin/assign_role.html', {"form": form})
-
-
-# @user_passes_test(is_admin, login_url='no-permission')
+@user_passes_test(is_admin, login_url='no-permission')
 def create_group(request):
     form = CreateGroupForm()
     if request.method == 'POST':
@@ -115,22 +98,9 @@ def create_group(request):
     return render(request, 'admin/create_group.html', {'form': form})
 
 
-# @user_passes_test(is_admin, login_url='no-permission')
+@user_passes_test(is_admin, login_url='no-permission')
 def group_list(request):
     groups = Group.objects.prefetch_related('permissions').all()
     return render(request, 'admin/group_list.html', {'groups': groups})
 
 
-""" 
-
-    Admin
-        - Sobkisui
-    Manager
-        - project
-        - task create
-    Employee
-        - Task read
-        - Task update
-    
-    Role Based Access Control (RBAC)
-"""
